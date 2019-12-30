9f8ceb5df7935eab2b60fdf5d63e543c638c58b2
sotramp <sotraamp@gmail.com>
2019-12-30 14:06:07 +0100

    project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Project Model
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date de parution")
+    
+    class Meta:
+        verbose_name = "project"
+        ordering = ['date']
+    
+    def __str__(self):
+        return self.name
