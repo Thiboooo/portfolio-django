373c504a8d9aca66e0b418874bb84518f3e72bd2
sotramp <sotraamp@gmail.com>
2020-01-01 17:48:31 +0100

    contact form

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,10 +5,3 @@ class ContactForm(forms.Form):
     contactName = forms.CharField(required=True)
     contactEmail = forms.EmailField(required=True)
     message = forms.CharField(required=True,widget=forms.Textarea)
-
-    # customization
-    def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['contactName'].label = "Votre nom"
-        self.fields['contactEmail'].label = "Votre adresse mail"
-        self.fields['message'].label ="Votre message.."
